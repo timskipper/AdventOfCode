3dceb3a0c5ec75d17d908d1c9b97f620c2a7c573
Tim Skipper <tim@intonet-technology.co.uk>
2022-12-03 12:08:42 +0000

    Day 2 Part 1

---
+++ DayTwo.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode
+{
+    public class DayTwo
+    {
+        private int totalScore;
+
+        public DayTwo(string dataFile)
+        {
+            totalScore = 0;
+            var input = File.ReadAllLines(dataFile);
+
+            foreach (var line in input)
+            {
+                var game = line.Split(' ');
+                totalScore += CalculateScore(game[0], game[1]);
+            }
+        }
+
+        public int Score => totalScore;
+
+        public int CalculateScore(string play, string response)
+        {
+            switch (play)
+            {
+                case "A": // Rock
+                    switch (response)
+                    {
+                        case "X": // Rock
+                            return 3 + 1;
+                        case "Y": // Paper
+                            return 6 + 2;
+                        case "Z": // Scissors
+                            return 0 + 3;
+                    }
+                    break;
+                case "B": // Paper
+                    switch (response)
+                    {
+                        case "X": // Rock
+                            return 0 + 1;
+                        case "Y": // Paper
+                            return 3 + 2;
+                        case "Z": // Scissors
+                            return 6 + 3;
+                    }
+                    break;
+                case "C": // Scissors
+                    switch (response)
+                    {
+                        case "X": // Rock
+                            return 6 + 1;
+                        case "Y": // Paper
+                            return 0 + 2;
+                        case "Z": // Scissors
+                            return 3 + 3;
+                    }
+                    break;
+            }
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿var dayOne = new DayOne("../../../day1input.txt");
+﻿using AdventOfCode;
+
+var dayOne = new DayOne("../../../day1input.txt");
 Console.WriteLine($"Elf with {dayOne.MostCalories} calories has the most.");
 Console.WriteLine($"Total of top 3 calories is {dayOne.TopThreeCalories}");
+
+var dayTwo = new DayTwo("../../../day2input.txt");
+Console.WriteLine($"Total score is {dayTwo.Score}");
