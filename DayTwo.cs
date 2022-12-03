adef2ea0a47ac2ab200f02157babfc6aa9c10b79
Tim Skipper <tim@intonet-technology.co.uk>
2022-12-03 13:11:21 +0000

    Day 2 Part 2

--- DayTwo.cs
+++ DayTwo.cs
@@ -2,21 +2,25 @@
 {
     public class DayTwo
     {
-        private int totalScore;
+        private int totalScore1;
+        private int totalScore2;
 
         public DayTwo(string dataFile)
         {
-            totalScore = 0;
+            totalScore1 = 0;
+            totalScore2 = 0;
             var input = File.ReadAllLines(dataFile);
 
             foreach (var line in input)
             {
                 var game = line.Split(' ');
-                totalScore += CalculateScore(game[0], game[1]);
+                totalScore1 += CalculateScore(game[0], game[1]);
+                totalScore2 += CalculateResponse(game[0], game[1]);
             }
         }
 
-        public int Score => totalScore;
+        public int Part1Score => totalScore1;
+        public int Part2Score => totalScore2;
 
         public int CalculateScore(string play, string response)
         {
@@ -53,6 +57,47 @@
                             return 0 + 2;
                         case "Z": // Scissors
                             return 3 + 3;
+                    }
+                    break;
+            }
+            return 0;
+        }
+
+        public int CalculateResponse(string play, string result)
+        {
+            switch (play)
+            {
+                case "A": // Rock
+                    switch (result)
+                    {
+                        case "X": // Lose
+                            return 0 + 3;
+                        case "Y": // Draw
+                            return 3 + 1;
+                        case "Z": // Win
+                            return 6 + 2;
+                    }
+                    break;
+                case "B": // Paper
+                    switch (result)
+                    {
+                        case "X": // Lose
+                            return 0 + 1;
+                        case "Y": // Draw
+                            return 3 + 2;
+                        case "Z": // Win
+                            return 6 + 3;
+                    }
+                    break;
+                case "C": // Scissors
+                    switch (result)
+                    {
+                        case "X": // Lose
+                            return 0 + 2;
+                        case "Y": // Draw
+                            return 3 + 3;
+                        case "Z": // Win
+                            return 6 + 1;
                     }
                     break;
             }
--- Program.cs
+++ Program.cs
@@ -5,4 +5,5 @@ Console.WriteLine($"Elf with {dayOne.MostCalories} calories has the most.");
 Console.WriteLine($"Total of top 3 calories is {dayOne.TopThreeCalories}");
 
 var dayTwo = new DayTwo("../../../day2input.txt");
-Console.WriteLine($"Total score is {dayTwo.Score}");
+Console.WriteLine($"Part 1 score is {dayTwo.Part1Score}");
+Console.WriteLine($"Part 2 score is {dayTwo.Part2Score}");
