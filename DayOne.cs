a844143ad8d74d0d9c292ad3facb2ac42d9ae389
Tim Skipper <tim@intonet-technology.co.uk>
2022-12-03 11:01:53 +0000

    Day 1 Part 1

---
+++ DayOne.cs
@@ -0,0 +1,49 @@
+﻿public class DayOne
+{
+    private readonly List<Elf> data;
+
+    public DayOne(string dataFile)
+    {
+        this.data = new List<Elf>();
+
+        var input = File.ReadAllLines(dataFile);
+
+        var elfId = 1;
+        foreach (var line in input)
+        {
+            if (line == string.Empty)
+            {
+                elfId++;
+                continue;
+            }
+
+            data.Add(new Elf(elfId, int.Parse(line)));
+        }
+    }
+
+    public int MostCalories => data
+        .GroupBy(
+            elf => elf.Id,
+            elf => elf.Calories,
+            (elf, elves) => new
+            {
+                Id = elf,
+                Calories = elves.Sum()
+            }
+        )
+        .OrderByDescending(x => x.Calories)
+        .First()
+        .Calories;
+}
+
+public class Elf
+{
+    public Elf(int id, int calories)
+    {
+        Id = id;
+        Calories = calories;
+    }
+
+    public int Id { get; set; }
+    public int Calories { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿var dayOne = new DayOne("../../../day1input.txt");
+Console.WriteLine($"Elf with {dayOne.MostCalories} calories has the most.");
